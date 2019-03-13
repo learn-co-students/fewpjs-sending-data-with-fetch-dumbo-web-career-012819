45101441eb10f0ceab43293cec7e82eee0a095da
Cristian Leiva <cleiva94.cl@gmail.com>
2019-03-13 19:41:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const testVar = {}
-
 function testFunc() {
   return "hi"
+}
+
+let formData = {
+  firstName: 'Cristian',
+  registryMessage: 'Hello'
+}
+
+
+const registerSelf = () => {
+  return fetch('http://guestbook.example.com/register', {
+    method: 'POST',
+    body: formData
+  }).then(function(res) {
+    return res.json()
+  }).then(function(content) {
+    return content.message
+  })
+} 
+
+const errorSelf = () => {
+  return fetch('http://guestbook.example.com/register-error')
+    .catch(function(error) {
+      return ":("
+    })
 }
