7335e5ee42fad47d36854ad4424fa1d8c0d8aa31
Nate <github email address>
2019-03-15 16:50:36 -0400

    tests passed

--- index.js
+++ index.js
@@ -3,3 +3,32 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function registerSelf() {
+  return fetch('http://guestbook.example.com/register', {
+    'method': 'POST',
+    'body': {
+      'firstName': 'Nate',
+      'registryMessage': "This is a message in a test in a course in life."
+    }
+  })
+    .then(response => response.json())
+    .then(data => data.message);
+}
+
+//
+
+
+
+function errorSelf() {
+  return fetch('http://guestbook.example.com/register-error', {
+    'method': 'POST',
+    'body': {
+      'firstName': 'Nate',
+      'registryMessage': "This is a message in a test in a course in life."
+    }
+  })
+  .then(response => response.json())
+  .then(data => data.message)
+  .catch(error => ":(")
+}
